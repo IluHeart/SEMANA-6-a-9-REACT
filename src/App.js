29e70459a8b2b29c1915c1f0ee5c7a090d492cc3
IliesGuellaf <108081986+IluHeart@users.noreply.github.com>
2023-10-04 14:18:00 +0200

    Ejercicio 2 React JSX

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  // Creamos la variable 'hola' con el mensaje
-  const hola = <h1>¡Hola, mundo!</h1>;
+function Saludo(props) {
+  const mensaje = `¡Hola, ${props.nombre}!`;
 
+  return <h1>{mensaje}</h1>;
+}
+
+function App() {
   return (
     <div className="App">
-      {/* Utilizamos la variable 'hola' en nuestro componente */}
-      {hola}
+      <Saludo nombre="Mundo" />
     </div>
   );
 }
