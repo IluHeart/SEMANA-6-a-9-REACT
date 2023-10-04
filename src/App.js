a964dd6e815ecc13a10e5314b4f4b4fc738c1afd
IliesGuellaf <108081986+IluHeart@users.noreply.github.com>
2023-10-04 14:54:11 +0200

    Ejercicio 5 React JSX

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import JSX1 from "./Ejercicios/Ejercicio 1.js";
 import JSX2 from "./Ejercicios/Ejercicio 2.js";
 import JSX3 from "./Ejercicios/Ejercicio 3.js";
 import JSX4 from "./Ejercicios/Ejercicio 4.js";
+import JSX5 from "./Ejercicios/Ejercicio 5.js";
 
 //Comence a agrupar los ejercicios como lo hizo wilmer porque me estuco ayudando y lo comence a hacer similar a el
 
@@ -13,6 +14,9 @@ function App() {
       <JSX2/>
       <JSX3/>
       <JSX4 name={<a href="https://instagram.com">Instagram</a>} />
+      <JSX5 
+      a={1010101}
+      b={1010101} />
 
     </div>
   );
---
+++ src/Ejercicios/Ejercicio 5.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const JSX5 = ({a, b }) => {
+    const sum = a + b;
+    return <h2>Sum: {sum}</h2>;
+  };
+
+export default JSX5;
