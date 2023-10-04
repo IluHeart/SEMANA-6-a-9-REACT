f718aae6a1d2e3a357da09dbe092224852b45d31
IliesGuellaf <108081986+IluHeart@users.noreply.github.com>
2023-10-04 14:40:35 +0200

    Ejercicio 1 2 Origanizados y Ejercicio 3 entregado React JSX

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import JSX1 from "./Ejercicios/Ejercicio 1.js";
 import JSX2 from "./Ejercicios/Ejercicio 2.js";
 import JSX3 from "./Ejercicios/Ejercicio 3.js";
 
-<div className="App">
-<JSX1/>
-<JSX2/>
-<JSX3/>
+//Comence a agrupar los ejercicios como lo hizo wilmer porque me estuco ayudando y lo comence a hacer similar a el
+function App() {
+  return (
+    <div className="App">
+      <JSX1/>
+      <JSX2/>
+      <JSX3/>
 
-</div>
-);
+    </div>
+  );
 }
+
+export default App;
